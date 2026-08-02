df04c031c3338daa568dd232028592dca93c61c8
swaaz <swaasthik.shetty07@gmail.com>
2026-08-02 14:41:42 +0100

    Add SEO improvements: sitemap, robots.txt, OG/Twitter tags, structured data

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,22 @@
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://www.googletagmanager.com" />
+          <link rel="preconnect" href="https://va.vercel-scripts.com" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
